e87a81f0880b9ec1f2d6bab3516da78cef2c848e
Malte-Maurice Dreyer <dreyer.maltem+dev@gmail.com>
2015-05-16 11:47:21 +0000

    Fix style errors

--- upgrade-object.js
+++ upgrade-object.js
@@ -7,13 +7,13 @@ export default function (obj, methods) {
     const propDesc = Object.getOwnPropertyDescriptor(obj, propName)
 
     if ('value' in propDesc) {
-      if(typeof propDesc.value === 'function') {
+      if (typeof propDesc.value === 'function') {
         propDesc.value = propDesc.value.bind(obj)
       }
-    } else if('get' in propDesc) {
+    } else if ('get' in propDesc) {
       const originalGet = propDesc.get
 
-      propDesc.get = function() {
+      propDesc.get = function () {
         const val = originalGet.call(this)
 
         if (typeof val === 'function') {
@@ -25,15 +25,15 @@ export default function (obj, methods) {
 
     if (methods.indexOf(propName) !== -1) {
       if ('value' in propDesc) {
-        if(typeof propDesc.value !== 'function') {
+        if (typeof propDesc.value !== 'function') {
           throw new TypeError(`Property ${propName} is not a function`)
         }
 
         propDesc.value = upgradeFunction(propDesc.value)
-      } else if('get' in propDesc) {
+      } else if ('get' in propDesc) {
         const originalGet = propDesc.get
 
-        propDesc.get = function() {
+        propDesc.get = function () {
           const val = originalGet.call(this)
 
           if (typeof val !== 'function') {
